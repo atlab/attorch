8a32e5ab3b5fe04a0fecaa60bc02b1975d8a9308
Fabian Sinz <sinz@bcm.edu>
2017-08-22 00:11:28 +0200

    bugfix

--- attorch/layers.py
+++ attorch/layers.py
@@ -108,7 +108,7 @@ class SpatialXFeatureLinear3D(nn.Module):
         # tmp = F.conv3d(x, self.constrainted_features, None)
         # tmp2 = F.conv3d(tmp, self.normalized_spatial, self.bias, groups=self.outdims).squeeze(4).squeeze(3)
         # return tmp2.transpose(2, 1)
-        return F.conv3d(x, self.weight, self.bias).squeeze(4).squeeze(3)
+        return F.conv3d(x, self.weight, self.bias).squeeze(4).squeeze(3).transpose(2,1)
 
     def __repr__(self):
         c, t, w, h = self.in_shape
