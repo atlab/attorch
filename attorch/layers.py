4cc8afd8161a5735b7b2a4e622e8fc1e1d18b690
Fabian Sinz <sinz@bcm.edu>
2017-09-10 22:02:47 +0200

    bugfix

--- attorch/layers.py
+++ attorch/layers.py
@@ -333,7 +333,7 @@ class DivNorm3d(nn.Module):
     def forward(self, x):
         mu = x.mean(1).mean(2).mean(3).mean(4)
         y = x - mu.expand_as(x)
-        y = y / torch.sqrt(self.sigma + y.pow(2).mean())
+        y = y / torch.sqrt(self.sigma + y.pow(2).mean().expand_as(y))
 
         if self.bias is not None:
             y = y + self.bias.expand_as(y)
