4309c8528dddee5d4fc938dcd1e2f69d904431b1
Fabian Sinz <sinz@bcm.edu>
2018-02-21 13:42:47 -0600

    bughunt in response extraction

--- attorch/dataloaders.py
+++ attorch/dataloaders.py
@@ -7,12 +7,14 @@ class RepeatsBatchSampler(Sampler):
     def __init__(self, keys, subset_index=None):
         if subset_index is None:
             subset_index = np.arange(len(keys))
-        _, inv = np.unique(keys[subset_index], return_inverse=True)
+        self.hashes, inv = np.unique(keys[subset_index], return_inverse=True)
         self.repeat_index = np.unique(inv)
+        self.hashes = self.hashes[self.repeat_index]
         self.repeat_sets = inv
         self.subset_index = subset_index
 
     def __iter__(self):
+        print(self.hashes)
         for u in self.repeat_index:
             yield list(self.subset_index[self.repeat_sets == u])
 
