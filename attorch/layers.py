bugfix for masking in PooledReadout3d

attorch/layers.py
@@ -599,7 +599,7 @@ class SpatialTransformerPooled3d(nn.Module):
         else:
             self.grid = grid
         self.features = Parameter(torch.Tensor(1, c * (self._pool_steps + 1), 1, outdims))
-        self.register_buffer('mask', 0 * self.features.data + 1)
+        self.register_buffer('mask', self.features.data.new(*self.features.size()).fill_(1.))
 
         if bias:
             bias = Parameter(torch.Tensor(outdims))
@@ -625,7 +625,7 @@ class SpatialTransformerPooled3d(nn.Module):
             outdims = self.outdims
             self._pool_steps = int(value)
             self.features = Parameter(torch.Tensor(1, c * (self._pool_steps + 1), 1, outdims))
-            self.mask =  0 * self.features.data + 1
+            self.mask = self.features.data.new(*self.features.size()).fill_(1.)
             self.features.data.fill_(1 / self.in_shape[0])
 
     def initialize(self, init_noise=1e-3, grid=True):