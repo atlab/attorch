401b01e32273e3a48f5b8d2fbbb1c11d02f09fb9
Fabian Sinz <sinz@bcm.edu>
2018-07-21 16:07:36 -0500

    new readout layer

--- attorch/layers.py
+++ attorch/layers.py
@@ -581,10 +581,6 @@ class SpatialTransformerPyramid3d(nn.Module):
 
 
 class SpatialTransformerPooled3d(nn.Module):
-    """
-    Factorized readout layer from convolution activations. For each feature layer, the readout weights are
-    Gaussian over spatial dimensions.
-    """
 
     def __init__(self, in_shape, outdims, pool_steps=1, positive=False, bias=True,
                  init_range=.05, kernel_size=2, stride=2, grid=None, stop_grad=False):
@@ -699,6 +695,130 @@ class SpatialTransformerPooled3d(nn.Module):
         for i in range(self._pool_steps):
             z = self.avg(z)
             pools.append(F.grid_sample(z, grid))
+        y = torch.cat(pools, dim=1)
+        y = (y.squeeze(-1) * feat).sum(1).view(N, t, outdims)
+
+        if self.bias is not None:
+            if subs_idx is None:
+                y = y + self.bias
+            else:
+                y = y + self.bias[subs_idx]
+
+        return y
+
+    def __repr__(self):
+        c, _, w, h = self.in_shape
+        r = self.__class__.__name__ + \
+            ' (' + '{} x {} x {}'.format(c, w, h) + ' -> ' + str(self.outdims) + ')'
+        if self.bias is not None:
+            r += ' with bias'
+        if self.stop_grad:
+            r += ', stop_grad=True'
+        r += '\n'
+
+        for ch in self.children():
+            r += '  -> ' + ch.__repr__() + '\n'
+        return r
+
+class SpatialTransformerXPooled3d(nn.Module):
+
+    def __init__(self, in_shape, outdims, pool_steps=1, positive=False, bias=True,
+                 init_range=.2, grid_points=10, kernel_size=4, stride=4, grid=None,
+                 stop_grad=False):
+        super().__init__()
+        self._pool_steps = pool_steps
+        self.in_shape = in_shape
+        c, t, w, h = in_shape
+        self.outdims = outdims
+        self.positive = positive
+        self._grid_points = grid_points
+        if grid is None:
+            self.grid = Parameter(torch.Tensor(1, outdims, grid_points, 2))
+        else:
+            self.grid = grid
+        self.features = Parameter(torch.Tensor(1, c * (self._pool_steps + 1), 1, outdims))
+
+        if bias:
+            bias = Parameter(torch.Tensor(outdims))
+            self.register_parameter('bias', bias)
+        else:
+            self.register_parameter('bias', None)
+
+        self.avg = nn.AvgPool2d(kernel_size, stride=stride, count_include_pad=False)
+        self.init_range = init_range
+        self.initialize()
+        self.stop_grad = stop_grad
+
+    @property
+    def pool_steps(self):
+        return self._pool_steps
+
+    @pool_steps.setter
+    def pool_steps(self, value):
+        assert value >= 0 and int(value) - value == 0, 'new pool steps must be a non-negative integer'
+        if value != self._pool_steps:
+            print('Resizing readout features')
+            c, t, w, h = self.in_shape
+            outdims = self.outdims
+            self._pool_steps = int(value)
+            self.features = Parameter(torch.Tensor(1, c * (self._pool_steps + 1), 1, outdims))
+            self.features.data.fill_(1 / self.in_shape[0])
+
+    def initialize(self, init_noise=1e-3, grid=True):
+        # randomly pick centers within the spatial map
+
+        self.features.data.fill_(1 / self.in_shape[0])
+        if self.bias is not None:
+            self.bias.data.fill_(0)
+        if grid:
+            self.grid.data.uniform_(-self.init_range, self.init_range)
+
+
+    def feature_l1(self, average=True, subs_idx=None):
+        subs_idx = subs_idx if subs_idx is not None else slice(None)
+        if average:
+            return self.features[..., subs_idx].abs().mean()
+        else:
+            return self.features[..., subs_idx].abs().sum()
+
+    def dgrid_l2(self, average=True, subs_idx=None):
+        subs_idx = subs_idx if subs_idx is not None else slice(None)
+        if average:
+            return (self.grid[:, subs_idx, :-1, :] - self.grid[:, subs_idx, 1:, :]).pow(2).mean()
+        else:
+            return (self.grid[:, subs_idx, :-1, :] - self.grid[:, subs_idx, 1:, :]).pow(2).sum()
+
+
+    def forward(self, x, shift=None, subs_idx=None):
+        if self.stop_grad:
+            x = x.detach()
+
+        if self.positive:
+            positive(self.features)
+        self.grid.data = torch.clamp(self.grid.data, -1, 1)
+
+        N, c, t, w, h = x.size()
+        m = self._pool_steps + 1
+        if subs_idx is not None:
+            feat = self.features[..., subs_idx].contiguous()
+            outdims = feat.size(-1)
+            feat = feat.view(1, m * c, outdims)
+            grid = self.grid[:, subs_idx, ...]
+        else:
+            grid = self.grid
+            feat = self.features.view(1, m * c, self.outdims)
+            outdims = self.outdims
+        if shift is None:
+            grid = grid.expand(N * t, outdims, self._grid_points, 2)
+        else:
+            grid = grid.expand(N, outdims, self._grid_points, 2)
+            grid = torch.stack([grid + shift[:, i, :][:, None, None, :] for i in range(t)], 1)
+            grid = grid.contiguous().view(-1, outdims, self._grid_points, 2)
+        z = x.contiguous().transpose(2, 1).contiguous().view(-1, c, w, h)
+        pools = [F.grid_sample(z, grid).mean(dim=3, keepdim=True)]
+        for i in range(self._pool_steps):
+            z = self.avg(z)
+            pools.append(F.grid_sample(z, grid).mean(dim=3, keepdim=True))
         y = torch.cat(pools, dim=1)
         y = (y.squeeze(-1) * feat).sum(1).view(N, t, outdims)
 
